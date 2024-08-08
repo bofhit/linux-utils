64183dd292f16e4708410aa298806de8b24d53a1
Tim <it@blessingsofhope.com>
2024-08-08 15:49:30 -0400

    Using Pandas for grid output.

--- docker/show_ports.py
+++ docker/show_ports.py
@@ -1,14 +1,71 @@
+import argparse
+
 import docker 
 from icecream import ic
+import pandas as pd
 
-client = docker.from_env()
+def main(**kwargs):
 
-containers = client.containers.list()
+    if 'sort_container_port' in kwargs:
+        sort_container_port = True
+    else:
+        sort_container_port = False
 
-ports = {}
+    if 'sort_host_port' in kwargs:
+        sort_host_port = True
+    else:
+        sort_host_port = False
 
-for c in containers:
-    ports[c.name] = c.ports
+    client = docker.from_env()
 
-ic(ports)
+    containers = client.containers.list()
 
+    ports = []
+
+    for c in containers:
+        for p in c.ports:
+            tmp = {}  
+            tmp['Container'] = c.name
+            tmp['Container Port'] = p
+            if ic(c.ports[p]):
+                for host_item in ic(c.ports[p]):
+                    tmp['Host Port'] = host_item['HostPort']
+                    tmp['Host IP'] = host_item['HostIp']
+                ports.append(tmp)
+            else:
+                tmp['Host Port'] = ''
+                tmp['Host IP'] = ''
+                ports.append(tmp)
+
+    df = pd.DataFrame(ports)
+
+    if sort_container_port:
+        df = df.sort_values('Container Port')
+
+    if sort_host_port:
+        df = df.sort_values('Host Port')
+
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+   
+    parser.add_argument('--sort_container_port', action='store_true')
+    parser.add_argument('--sort_host_port', action='store_true')
+
+
+    args = parser.parse_args()
+
+    kwargs = {}
+    if args.sort_container_port:
+        kwargs['sort_container_port'] = args.sort_container_port
+
+    if args.sort_host_port:
+        kwargs['sort_host_port'] = args.sort_host_port
+
+    res = main(
+       **kwargs 
+    )  
+
+    print(res)  
+    
